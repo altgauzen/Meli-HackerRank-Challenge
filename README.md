49035500e1563afa310c0062529bb7cea2715247
Alexandre Altgauzen <alexandrealtgauzen@Alexandres-MacBook-Air.local>
2022-05-15 19:15:41 +0100

    feat: challenge Meli HackerRank

---
+++ README.md
@@ -0,0 +1,14 @@
+<h2 align="center">
+  Meli Challenge - Decompose Numbers
+</h2>
+
+<p align="center">
+  Código executado em javascript no desafio proposto pelo Mercado Livre na plataforma HackerRank. Para efeitos de estudo e prática.</a>
+</p>
+## ➕ O Desafio:
+
+ Complete a seguinte função para que a mesma devolva todos os possíveis números
+ de 4 dígitos, onde cada um seja menor ou igual a <maxDigit>,
+ e a soma dos dígitos de cada número gerado seja 21.
+ Exemplo com maxDigit=6: 3666
+                         4566
---
+++ meliChallengeDecomposeNumbers.js
@@ -0,0 +1,32 @@
+// Complete a seguinte função para que a mesma devolva todos os possíveis números
+// de 4 dígitos, onde cada um seja menor ou igual a <maxDigit>,
+// e a soma dos dígitos de cada número gerado seja 21
+// Exemplo com maxDigit=6: 3666
+//                         4566
+
+function decomposeNumbers(maxDigit) {
+  let arrayValidValues = [];
+  let maxValue = maxDigit + maxDigit*10 + maxDigit*100 + maxDigit*1000;
+  let minValue = 1000;
+  
+  for(let i = minValue; i < maxValue; i++) {
+      let stringValue = i.toString();
+      let arr = stringValue.split("");
+      
+      if((arr[0] <= maxDigit) && (arr[1] <= maxDigit) && (arr[2] <= maxDigit) && arr[3] <= maxDigit) {
+          let sum = parseInt(arr[0]) + parseInt(arr[1]) + parseInt(arr[2]) + parseInt(arr[3]);
+          if (sum == 21) {
+              arrayValidValues.push(arr.join(""));
+          }
+      }
+  }
+  if (arrayValidValues.length > 0) {
+    for (i = 0; i < arrayValidValues.length; i++){
+	    console.log(arrayValidValues[i]);
+    }
+  } else {
+    console.log(null);
+  }
+}
+
+decomposeNumbers(6);
